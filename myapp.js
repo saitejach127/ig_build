aaf55913924336da5cc8656dc784466276eb1a94
saitejach127 <saitejach127@gmail.com>
2020-10-02 10:46:17 +0000

    Update from https://github.com/WebdevteamIG/igconnect-frontend/commit/1ead631bd4ae08739a735948ae638c6601ad890e

---
+++ myapp.js
@@ -0,0 +1,5 @@
+console.log("hello work")
+
+function test() {
+  console.log("Whats app");
+}
---
+++ precache-manifest.e18754b34567bc2169da0a81beeca488.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144bc80078c030eadd6948c9480a8c52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae44a267bcf8e1ed431d",
+    "url": "/static/css/2.2d63d0b4.chunk.css"
+  },
+  {
+    "revision": "90f2e78d0c7f2c82a743",
+    "url": "/static/css/main.da7a04e1.chunk.css"
+  },
+  {
+    "revision": "ae44a267bcf8e1ed431d",
+    "url": "/static/js/2.d4ae0181.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d4ae0181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f2e78d0c7f2c82a743",
+    "url": "/static/js/main.d4718756.chunk.js"
+  },
+  {
+    "revision": "d70aa2ba7df362d6f5d8",
+    "url": "/static/js/runtime-main.5e0920cb.js"
+  },
+  {
+    "revision": "61a8695dbe86bd3ee8ecb7ab522d3732",
+    "url": "/static/media/Gitworkshop.61a8695d.jpeg"
+  },
+  {
+    "revision": "34621355e5353cb6dc62ce5734ec340e",
+    "url": "/static/media/OICposter.34621355.jpeg"
+  },
+  {
+    "revision": "6d4b8344ae8bf791203eebec7dc54943",
+    "url": "/static/media/igpic.6d4b8344.png"
+  },
+  {
+    "revision": "db35683038441b71194a72416e2b8b70",
+    "url": "/static/media/nitwlogo.db356830.png"
+  }
+]);

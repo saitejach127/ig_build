f97be1c2aaeab12f53628e1eb698095eabd5be81
saitejach127 <saitejach127@gmail.com>
2020-10-08 16:56:04 +0000

    Update from https://github.com/WebdevteamIG/igconnect-frontend/commit/6e51c43ca1a5a9f43abb78a017093e4f2812f60b

--- myapp.js
+++
@@ -1,5 +0,0 @@
-console.log("hello work")
-
-function test() {
-  console.log("Whats app");
-}
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cii/precache-manifest.85ec04c0ac51575b2e832c5da1d518c7.js"
+  "/cii/precache-manifest.282caa41a6ffb4142c4ef7492fafa39e.js"
 );
 
 self.addEventListener('message', (event) => {

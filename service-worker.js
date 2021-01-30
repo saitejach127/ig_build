aaa39e9c5a773c70c5621b2433226bd36ccb417e
saitejach127 <saitejach127@gmail.com>
2021-01-30 15:44:26 +0000

    Update from https://github.com/WebdevteamIG/igconnect-frontend/commit/d362f3c2ba1e82f51562ea8b36148832970216e5

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cii/precache-manifest.0ccc5dbe3c4425d1e1d8e50ee82c2702.js"
+  "/cii/precache-manifest.35b2693f74394c79319b56bf90740e9e.js"
 );
 
 self.addEventListener('message', (event) => {
